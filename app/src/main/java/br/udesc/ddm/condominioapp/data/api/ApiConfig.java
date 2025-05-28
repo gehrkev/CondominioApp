96687211f6441a74be1f38414b94ca3ffad76445
Vitor André Gehrke <vitor.gehrke@gmail.com>
2025-05-28 00:14:55 -0300

    Usando gradle.properties para guardar a chave API

--- app/src/main/java/br/udesc/ddm/condominioapp/data/api/ApiConfig.java
+++ app/src/main/java/br/udesc/ddm/condominioapp/data/api/ApiConfig.java
@@ -1,10 +1,13 @@
 package br.udesc.ddm.condominioapp.data.api;
 
+import br.udesc.ddm.condominioapp.BuildConfig;
+
 public class ApiConfig {
 
     public static final String VIA_CEP_BASE_URL = "https://viacep.com.br/";
     public static final String GOOGLE_MAPS_BASE_URL = "https://maps.googleapis.com/";
-    public static final String GOOGLE_MAPS_API = "CHAVE_API_AQUI";
+
+    public static final String GOOGLE_MAPS_API = BuildConfig.GOOGLE_MAPS_API_KEY;
 
     public static final int DEFAULT_CONNECT_TIMEOUT = 30;
     public static final int DEFAULT_READ_TIMEOUT = 30;
